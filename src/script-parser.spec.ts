26988f837af0f58488378827bc1895a7e0afbd70
Joe White <108666+joewhite@users.noreply.github.com>
2022-04-21 04:24:49 +0000

    Parse the entire script including indentation

--- src/script-parser.spec.ts
+++ src/script-parser.spec.ts
@@ -1,4 +1,4 @@
-import { parseScript } from "./script-parser";
+import { parseScript, ScriptStatement } from "./script-parser";
 
 describe("parseScript", () => {
     it("works for empty file", () => {
@@ -7,14 +7,63 @@ describe("parseScript", () => {
     it("ignores comments", () => {
         expect(parseScript("# This is a comment")).toStrictEqual([]);
     });
-    it("works for single statement", () => {
-        expect(parseScript("result = 0")).toStrictEqual([
+    it("parses flat list of statements", () => {
+        expect(parseScript("end", "end")).toStrictEqual([
+            { type: "end" },
+            { type: "end" },
+        ]);
+    });
+    it("parses one level of nesting", () => {
+        expect(
+            parseScript("if a", "  end", "  end", "if b", "  end")
+        ).toStrictEqual<ScriptStatement[]>([
             {
-                type: "assignment",
-                lvalue: "result",
-                operator: "=",
-                rvalue: "0",
+                type: "conditional",
+                keyword: "if",
+                condition: "a",
+                children: [{ type: "end" }, { type: "end" }],
             },
+            {
+                type: "conditional",
+                keyword: "if",
+                condition: "b",
+                children: [{ type: "end" }],
+            },
+        ]);
+    });
+    it("parses multiple levels of nesting", () => {
+        expect(
+            parseScript(
+                "if a",
+                "  if b",
+                "    if c",
+                "      end",
+                "  end",
+                "end"
+            )
+        ).toStrictEqual([
+            {
+                type: "conditional",
+                keyword: "if",
+                condition: "a",
+                children: [
+                    {
+                        type: "conditional",
+                        keyword: "if",
+                        condition: "b",
+                        children: [
+                            {
+                                type: "conditional",
+                                keyword: "if",
+                                condition: "c",
+                                children: [{ type: "end" }],
+                            },
+                        ],
+                    },
+                    { type: "end" },
+                ],
+            },
+            { type: "end" },
         ]);
     });
 });
--- src/script-parser.ts
+++ src/script-parser.ts
@@ -1,15 +1,59 @@
 import { parseLine, Statement } from "./line-parser";
 
-export type ScriptStatement = Statement & { children?: Statement[] };
+export type ScriptStatement = Statement & { children?: ScriptStatement[] };
+
+interface LineModel {
+    indent: number;
+    statement: Statement;
+    children: LineModel[];
+}
+
+function getIndentSize(line: string): number {
+    const indentMatch = /^ */.exec(line);
+    return indentMatch ? indentMatch[0].length : 0;
+}
+
+function lineModelToScriptStatement(lineModel: LineModel): ScriptStatement {
+    return lineModel.children.length
+        ? {
+              ...lineModel.statement,
+              children: lineModel.children.map(lineModelToScriptStatement),
+          }
+        : lineModel.statement;
+}
 
 export function parseScript(...lines: string[]): ScriptStatement[] {
-    const model: Statement[] = [];
+    const script: LineModel[] = [];
+    const currentIndents: LineModel[] = [];
+
     for (const line of lines) {
         const statement = parseLine(line);
         if (statement) {
-            model.push(statement);
+            const indent = getIndentSize(line);
+
+            const lineModel = {
+                indent,
+                statement,
+                children: [],
+            };
+
+            while (
+                currentIndents[currentIndents.length - 1]?.indent >= indent
+            ) {
+                currentIndents.pop();
+            }
+
+            if (currentIndents.length) {
+                currentIndents[currentIndents.length - 1].children.push(
+                    lineModel
+                );
+            } else {
+                script.push(lineModel);
+            }
+
+            currentIndents.push(lineModel);
         }
     }
 
-    return model;
+    return script.map(lineModelToScriptStatement);
 }
