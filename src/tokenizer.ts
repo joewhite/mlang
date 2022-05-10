68106660fddca817f9f16793c8869980c25335a7
Joe White <108666+joewhite@users.noreply.github.com>
2022-05-10 04:04:40 +0000

    Extract tokenizer.ts

---
+++ src/tokenizer.ts
@@ -0,0 +1,62 @@
+import {
+    assignmentOperators,
+    binaryOperators,
+    parentheticalOperators,
+} from "./operators";
+
+const operatorsLongestFirst: readonly string[] = (function () {
+    const tokens = [
+        ...assignmentOperators,
+        ...parentheticalOperators,
+        ...Object.keys(binaryOperators),
+    ];
+    tokens.sort((a, b) => {
+        // Reversed - longest first
+        if (a.length < b.length) {
+            return 1;
+        }
+
+        if (a.length > b.length) {
+            return -1;
+        }
+
+        return 0;
+    });
+    return tokens;
+})();
+
+function nextToken(line: string): string | undefined {
+    const numberMatches = /^(\d+(\.\d+)?)/.exec(line);
+    if (numberMatches) {
+        return numberMatches[1];
+    }
+
+    const identifierMatches = /^(\w+)/.exec(line);
+    if (identifierMatches) {
+        return identifierMatches[1];
+    }
+
+    for (const token of operatorsLongestFirst) {
+        if (line.startsWith(token)) {
+            return token;
+        }
+    }
+
+    return undefined;
+}
+
+export function lineToTokens(line: string): string[] {
+    const results: string[] = [];
+
+    while (line !== "") {
+        const match = nextToken(line);
+        if (match) {
+            results.push(match);
+            line = line.substring(match.length).trim();
+        } else {
+            throw new Error("Unexpected token at: " + line);
+        }
+    }
+
+    return results;
+}
